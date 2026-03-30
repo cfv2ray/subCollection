35a16913eda739640e60f94f1491d3a04ce8557a
jinwei26 <62389857+jinwei26@users.noreply.github.com>
2026-03-30 09:51:31 +0800

    zdfvgzxcdv

--- shareclash.py
+++ shareclash.py
@@ -41,7 +41,7 @@ def gets():
             # 打印成功提示
             print(f"✅ 下载成功：{url}")
         else:
-            print(f"❌ 连接失败，状态码：{response.status_code},URL:{url})  
+            print(f"❌ 连接失败，状态码：{response.status_code},URL:{url}")  
     except Exception as e:
         print("❌ 下载出错：{str(e)}")
 
